3b294b12f892ba732cbd1c20a93cecafe46947f5
Kevin <tkin1211@gmail.com>
2023-05-14 15:19:21 +0900

    SVG now has features to add nodes, drag nodes and add links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
+import React from "react";
+// import data from "./data/data.json";
+import Graph from "./components/Graph";
+import * as d3 from "d3";
+import { useMemo, useState } from "react";
 import "./App.css";
 
-import GraphPractice from "./components/GraphPractice";
-
-function App() {
+export default function App() {
   return (
-    <div>
-      <svg id="container" width="800" height="800"></svg>
-      <GraphPractice />
+    <div className="App">
+      <Graph />
+      {/* <svg xmlns="http://www.w3.org/2000/svg" width="313" height="305">
+        <path d="m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47" />
+      </svg> */}
     </div>
   );
 }
-
-export default App;
--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -1,97 +1,145 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import * as d3 from "d3";
 
 const Graph = () => {
   const svgRef = useRef(null);
-  const nodes = [];
-  const links = [];
+  const simulationRef = useRef(null);
+  // const [nodes, setNodes] = useState([
+  //   { id: "node1" },
+  //   { id: "node2" },
+  //   { id: "node3" },
+  // ]);
+  // const [links, setLinks] = useState([
+  //   { source: "node1", target: "node2" },
+  //   { source: "node2", target: "node3" },
+  // ]);
+  const [nodes, setNodes] = useState([]);
+  const [links, setLinks] = useState([]);
+  const [selectedNodes, setSelectedNodes] = useState([]);
 
   useEffect(() => {
+    // create a D3 simulation with a force-directed layout
+    const simulation = d3
+      .forceSimulation(nodes)
+      .force(
+        "link",
+        d3
+          .forceLink(links)
+          .id((d) => d.id)
+          .strength(0)
+      )
+      .force("charge", d3.forceManyBody().strength(0))
+      // .force("center", d3.forceCenter(300, 300))
+      .on("tick", () => {
+        // update the positions of the nodes and links on each tick of the simulation
+        nodesSelection.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+
+        linksSelection
+          .attr("x1", (d) => d.source.x)
+          .attr("y1", (d) => d.source.y)
+          .attr("x2", (d) => d.target.x)
+          .attr("y2", (d) => d.target.y);
+      });
+
+    // save the simulation to a ref for later use
+    simulationRef.current = simulation;
+
+    // create a D3 selection for the nodes and links
     const svg = d3.select(svgRef.current);
-
-    // Add the nodes
-    const node = svg
-      .selectAll(".node")
-      .data(nodes)
-      .enter()
-      .append("circle")
-      .attr("r", 10)
-      .attr("fill", "blue")
-      .call(drag());
-
-    // Add the edges
-    const link = svg
+    const linksSelection = svg
+      .select(".links")
       .selectAll(".link")
       .data(links)
-      .enter()
-      .append("line")
-      .attr("stroke", "gray")
+      .join("line")
+      .attr("class", "link")
+      .attr("stroke", "#999")
       .attr("stroke-width", 2);
-    let selectedNode = null;
 
-    // Add a click handler to add new nodes
+    const nodesSelection = svg
+      .select(".nodes")
+      .selectAll(".node")
+      .data(nodes)
+      .join("circle")
+      .attr("class", "node")
+      .attr("r", 20)
+      .attr("id", (d) => d.id)
+      .call(drag(simulation));
+
+    // const nodeRadius = 20;
+    // const nodesSelection = svg
+    //   .selectAll(".node")
+    //   .data(nodes, (node) => node.id)
+    //   .join("g")
+    //   .attr("class", "node")
+    //   .append("image")
+    //   .attr("x", (node) => node.x - nodeRadius)
+    //   .attr("y", (node) => node.y - nodeRadius)
+    //   .attr("width", nodeRadius * 2)
+    //   .attr("height", nodeRadius * 2)
+    //   .attr("xlink:href", "./house.svg")
+    //   .call(drag(simulation));
+
     svg.on("click", (event) => {
-      console.log("Clicked on SVG");
+      const targetNode = d3.select(event.target).node();
 
-      const newNode = {
-        id: nodes.length + 1,
-        x: event.offsetX,
-        y: event.offsetY,
-      };
-      nodes.push(newNode);
-      node
-        .data(nodes)
-        .enter()
-        .append("circle")
-        .attr("r", 10)
-        .attr("fill", "blue")
-        .call(drag());
-      if (event.target.nodeName === "circle") {
-        const clickedNode = d3.select(event.target);
-        if (!selectedNode) {
-          selectedNode = clickedNode;
+      if (targetNode.nodeName === "svg") {
+        // Clicked on empty space
+        console.log("svg clicked");
+        const point = d3.pointer(event);
+        const id = `node${nodes.length + 1}`;
+        const newNode = {
+          id: id,
+          x: point[0],
+          y: point[1],
+        };
+        setNodes([...nodes, newNode]);
+        setSelectedNodes([]);
+      } else if (targetNode.classList.contains("node")) {
+        // Clicked on a node
+        const nodeId = targetNode.id;
+
+        if (selectedNodes.length === 0) {
+          // First node selected
+          setSelectedNodes([nodeId]);
+        } else if (selectedNodes.length === 1 && selectedNodes[0] !== nodeId) {
+          // Second node selected, create link if it doesn't exist
+          const linkExists = links.some((link) => {
+            return (
+              (link.source === selectedNodes[0] && link.target === nodeId) ||
+              (link.source === nodeId && link.target === selectedNodes[0])
+            );
+          });
+
+          if (!linkExists) {
+            const newLink = { source: selectedNodes[0], target: nodeId };
+            setLinks([...links, newLink]);
+          }
+
+          setSelectedNodes([]);
         } else {
-          const newLink = {
-            source: selectedNode.data()[0].id,
-            target: clickedNode.data()[0].id,
-          };
-          links.push(newLink);
-          link
-            .data(links)
-            .enter()
-            .append("line")
-            .attr("stroke", "gray")
-            .attr("stroke-width", 2);
-          selectedNode = null;
+          // Clicked on the same node again, reset selection
+          setSelectedNodes([]);
         }
-      } else {
-        selectedNode = null;
       }
     });
 
-    // Add a click handler to add new edges
-    // Define the drag behavior for the nodes
-    function drag() {
+    // define a D3 drag behavior
+    function drag(simulation) {
       function dragstarted(event, d) {
-        d3.select(this).raise().classed("active", true);
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
       }
 
       function dragged(event, d) {
-        d.x = event.x;
-        d.y = event.y;
-        d3.select(this).attr("cx", d.x).attr("cy", d.y);
-        link
-          .filter((l) => l.source === d.id)
-          .attr("x1", d.x)
-          .attr("y1", d.y);
-        link
-          .filter((l) => l.target === d.id)
-          .attr("x2", d.x)
-          .attr("y2", d.y);
+        d.fx = event.x;
+        d.fy = event.y;
       }
 
       function dragended(event, d) {
-        d3.select(this).classed("active", false);
+        if (!event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
       }
 
       return d3
@@ -100,17 +148,20 @@ const Graph = () => {
         .on("drag", dragged)
         .on("end", dragended);
     }
-  }, []);
+
+    return () => {
+      // stop the simulation and remove the nodes and links when the component unmounts
+      simulation.stop();
+      nodesSelection.remove();
+      linksSelection.remove();
+    };
+  }, [nodes, links, selectedNodes]);
 
   return (
-    <svg
-      ref={svgRef}
-      width="100%"
-      height="100%"
-      viewBox="0 0 400 400"
-      style={{ display: "block" }}
-    >
-      <rect width="100%" height="100%" fill="grey" />
+    <svg ref={svgRef} width={600} height={600} style={{ border: "1px solid" }}>
+      <g class="links"></g>
+
+      <g class="nodes"></g>
     </svg>
   );
 };
