46158799a3f9e21e429e168aed18774ad002a4d0
Kevin <tkin1211@gmail.com>
2023-05-17 16:09:09 +0900

    Made Basic column structure main(graph, node-info), plant-info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-// import data from "./data/data.json";
-import Graph from "./components/Graph";
-import * as d3 from "d3";
 import { useMemo, useState } from "react";
 import "./App.css";
+
+import Graph from "./components/Graph";
+import Curve from "./components/Curve";
 
 export default function App() {
   const [nodes, setNodes] = useState([]);
@@ -11,13 +11,24 @@ export default function App() {
 
   return (
     <div className="App">
-      <Graph
-        nodes={nodes}
-        setNodes={setNodes}
-        links={links}
-        setLinks={setLinks}
-      />
-      {console.log(nodes, links)}
+      <div className="main">
+        <div className="graph">
+          <h2>Interactive Graph</h2>
+          <Graph
+            nodes={nodes}
+            setNodes={setNodes}
+            links={links}
+            setLinks={setLinks}
+          />
+        </div>
+        <div className="node-info">
+          <h2>Node Information</h2>
+          <Curve />
+        </div>
+      </div>
+      <div className="plant-info">
+        <h2>Plant Information</h2>
+      </div>
     </div>
   );
 }
