76ae81ca5b3d64a3414eb1299bf6c1859a62c673
Kevin <tkin1211@gmail.com>
2023-05-22 17:26:33 +0900

    Componentized NodeInfo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import NodeInfo from "./components/NodeInfo";
 import Graph from "./components/Graph";
-import Curve from "./components/Curve";
 
 export default function App() {
   const [nodes, setNodes] = useState([]);
@@ -21,42 +20,7 @@ export default function App() {
             setLinks={setLinks}
           />
         </div>
-        <div className="node-info">
-          <h2>Node Information</h2>
-          <div class="panel">
-            <div class="data-column">
-              <p>
-                <strong>ID:</strong> node1
-              </p>
-              <p>
-                <strong>Price Sensitivity:</strong> 0.87
-              </p>
-              <p>
-                <strong>Location:</strong>
-              </p>
-              <ul>
-                <li>
-                  <strong>X:</strong> 123
-                </li>
-                <li>
-                  <strong>Y:</strong> 199
-                </li>
-              </ul>
-            </div>
-            <div class="usage-column">
-              <p>
-                <strong>Current:</strong> 17 watts
-              </p>
-              <p>
-                <strong>Daily Average:</strong> 8 kWh
-              </p>
-              <p>
-                <strong>Distribution:</strong>
-              </p>
-              <Curve />
-            </div>
-          </div>
-        </div>
+        <NodeInfo />
       </div>
       <div className="plant-info">
         <h2>Plant Information</h2>
--- frontend/src/components/NodeInfo.js
+++ frontend/src/components/NodeInfo.js
@@ -1,20 +1,45 @@
 import React from "react";
-
+import Curve from "./Curve";
 function NodeInfo(nodes, selectedNodes) {
   // props = node
   const nodeSelected = { id: 1, priceSensitivity: 0.7, x: 123.123, y: 321.321 };
 
   return (
-    <div class="NodeInfo">
-      <p>Node Info Panel</p>
-      <pre>
-        {`
-          Selected node: ${nodeSelected.id}
-          Price Sensitivity: ${nodeSelected.priceSensitivity}
-          Location X: ${nodeSelected.x}
-          Location Y: ${nodeSelected.y}
-      `}
-      </pre>
+    <div className="node-info">
+      <h2>Node Information</h2>
+      <div class="panel">
+        <div class="data-column">
+          <p>
+            <strong>ID:</strong> ${nodeSelected.id}
+          </p>
+          <p>
+            <strong>Price Sensitivity:</strong> ${nodeSelected.priceSensitivity}
+          </p>
+          <p>
+            <strong>Location:</strong>
+          </p>
+          <ul>
+            <li>
+              <strong>X:</strong> ${nodeSelected.x}
+            </li>
+            <li>
+              <strong>Y:</strong> ${nodeSelected.y}
+            </li>
+          </ul>
+        </div>
+        <div class="usage-column">
+          <p>
+            <strong>Current:</strong> 17 watts
+          </p>
+          <p>
+            <strong>Daily Average:</strong> 8 kWh
+          </p>
+          <p>
+            <strong>Distribution:</strong>
+          </p>
+          <Curve />
+        </div>
+      </div>
     </div>
   );
 }
